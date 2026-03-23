f3f2caf02d4b86fd3aba92ca8d06dc5e56c41b2a
Ajay Marampalli <ajaymarampally99@gmail.com>
2026-03-22 23:41:26 -0400

    fix: updated logger to disable pino-pretty in netlify envs

--- src/logger.js
+++ src/logger.js
@@ -1,20 +1,28 @@
 const pino = require('pino');
 
+function shouldUsePrettyTransport() {
+  if (process.env.NODE_ENV === 'production') return false;
+  if (process.env.NETLIFY) return false;
+  if (process.env.AWS_LAMBDA_FUNCTION_NAME) return false;
+  return true;
+}
+
 function createLogger(level) {
+  const transport = shouldUsePrettyTransport()
+    ? {
+        target: require.resolve('pino-pretty'),
+        options: {
+          colorize: true,
+          singleLine: true,
+          translateTime: 'SYS:standard',
+        },
+      }
+    : undefined;
+
   return pino({
     level,
     timestamp: pino.stdTimeFunctions.isoTime,
-    transport:
-      process.env.NODE_ENV !== 'production'
-        ? {
-            target: 'pino-pretty',
-            options: {
-              colorize: true,
-              singleLine: true,
-              translateTime: 'SYS:standard',
-            },
-          }
-        : undefined,
+    transport,
   });
 }
 
